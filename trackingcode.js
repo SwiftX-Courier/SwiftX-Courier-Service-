2825ab6379f2f2295cfbe1205b60eec982ede204
SwiftX-Courier <benefit1guy@gmail.com>
2026-08-27 13:28:15 +0100

    Update trackingcode.js

--- trackingcode.js
+++ trackingcode.js
@@ -3,13 +3,13 @@
 const trackingData = {
     "ZAF0050738911": {
         trackingNumber: "ZAF0050738911",
-        name: "Keahilwe mokwena",
+        name: "Nicholson Kagiso",
         country: "south africa",
-        city: " Johannesburg, Gaunteng province",
-        addres: "House number number 6590 braamfischerville phase 2",
+        city: " Pretoria, Gaunteng province",
+        addres: "27831 Mashiloane Street Mamelodi East EXT5",
         shipmentstatus: "IN Transit",
-        shippingdate: "2026-18-08",
-        deliverytime: "2026-22-08 08:41:21 AM"
+        shippingdate: "2026-27-08",
+        deliverytime: "2026-31-08 08:41:21 AM"
     },
     
     "ZAF005073891111": {
